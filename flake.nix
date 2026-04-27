8877992cf58eb0a7ea173d7a6523f74467d4c023
John Carrijo <johncarrijo2255@gmail.com>
2026-04-27 11:34:39 -0300

    Novo flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "C++";
+  description = "C++ with OpenCV (GTK support)";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
@@ -7,21 +7,27 @@
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    # Define a versão personalizada do OpenCV
+    opencv4-gtk = pkgs.opencv4.override {
+      enableGtk3 = true;
+    };
   in {
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = [
-          pkgs.gcc
-          pkgs.gdb
-          pkgs.cmake
-          pkgs.ninja
-          pkgs.clang-tools
-          pkgs.valgrind
-
-          #Bibliotecas uteis do c++
-          pkgs.opencv4
-          pkgs.xtensor
-          pkgs.libtorch-bin
+        pkgs.gcc
+        pkgs.gdb
+        pkgs.cmake
+        pkgs.ninja
+        pkgs.clang-tools
+        pkgs.valgrind
+        opencv4-gtk   # <--- use a versão com suporte a GTK
+        pkgs.xtensor
+        pkgs.libtorch-bin
       ];
+      shellHook = ''
+        export DISPLAY=:0   # se necessário
+        echo "Ambiente C++ com OpenCV (GTK) pronto"
+      '';
     };
   };
 }
